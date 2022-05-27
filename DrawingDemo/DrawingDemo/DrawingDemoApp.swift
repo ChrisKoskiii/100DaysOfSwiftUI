71e1f09213b2a0133c87980e09eaee9281bf5393
Christopher Koski <82855685+ChrisKoskiii@users.noreply.github.com>
2022-05-27 12:22:28 -0400

    drawing demo initial commit

---
+++ DrawingDemo/DrawingDemo/DrawingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingDemoApp.swift
+//  DrawingDemo
+//
+//  Created by Christopher Koski on 5/27/22.
+//
+
+import SwiftUI
+
+@main
+struct DrawingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
