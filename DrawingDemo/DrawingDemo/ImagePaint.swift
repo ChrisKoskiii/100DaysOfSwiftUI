fe1bee1cad51f0cd8cbbf665bf5e65d6a6c9ae8c
Christopher Koski <82855685+ChrisKoskiii@users.noreply.github.com>
2022-06-09 08:16:53 -0400

    update to drawing

---
+++ DrawingDemo/DrawingDemo/ImagePaint.swift
@@ -0,0 +1,30 @@
+//
+//  ImagePain.swift
+//  DrawingDemo
+//
+//  Created by Christopher Koski on 6/9/22.
+//
+
+import SwiftUI
+//
+//struct ImagePaint: View {
+//  var body: some View {
+//    Text("Hello World")
+//        .frame(width: 300, height: 300)
+//        .border(ImagePaint(image: Image("Example"), scale: 0.2), width: 30)
+//  }
+//}
+//
+////struct CapsuleExample: View {
+////  var body: some View {
+////    Capsule()
+////      .strokeBorder(ImagePaint(image: Image("Example"), scale: 0.1), lineWidth: 20)
+////      .frame(width: 300, height: 200)
+////  }
+////}
+//
+//struct ImagePaint_Previews: PreviewProvider {
+//  static var previews: some View {
+//    ImagePaint()
+//  }
+//}
