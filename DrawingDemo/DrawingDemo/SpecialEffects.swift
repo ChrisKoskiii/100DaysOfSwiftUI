9efba252a1035b8ccb7283e5ae320c0b05d69dd9
Christopher Koski <82855685+ChrisKoskiii@users.noreply.github.com>
2022-06-14 13:50:07 -0400

    Special effects file added

---
+++ DrawingDemo/DrawingDemo/SpecialEffects.swift
@@ -0,0 +1,48 @@
+//
+//  SpecialEffects.swift
+//  DrawingDemo
+//
+//  Created by Christopher Koski on 6/14/22.
+//
+
+import SwiftUI
+
+struct SpecialEffects: View {
+  @State private var amount = 0.0
+  
+  var body: some View {
+    VStack {
+      ZStack {
+        Circle()
+          .fill(.red)
+          .frame(width: 200 * amount)
+          .offset(x: -50, y: -80)
+          .blendMode(.screen)
+        
+        Circle()
+          .fill(.green)
+          .frame(width: 200 * amount)
+          .offset(x: 50, y: -80)
+          .blendMode(.screen)
+        
+        Circle()
+          .fill(.blue)
+          .frame(width: 200 * amount)
+          .blendMode(.screen)
+      }
+      .frame(width: 300, height: 300)
+      
+      Slider(value: $amount)
+        .padding()
+    }
+    .frame(maxWidth: .infinity, maxHeight: .infinity)
+    .background(.black)
+    .ignoresSafeArea()
+  }
+}
+
+struct SpecialEffects_Previews: PreviewProvider {
+  static var previews: some View {
+    SpecialEffects()
+  }
+}
