38ac41c38b84a8490f00746c0d26b54ae74401fc
Christopher Koski <82855685+ChrisKoskiii@users.noreply.github.com>
2022-07-01 13:53:13 -0400

    finish Bookworm & challenges

---
+++ BookWorm/BookWorm/DetailView.swift
@@ -0,0 +1,95 @@
+//
+//  DetailView.swift
+//  BookWorm
+//
+//  Created by Christopher Koski on 7/1/22.
+//
+
+import SwiftUI
+import CoreData
+
+struct DetailView: View {
+  @Environment(\.managedObjectContext) var moc
+  @Environment(\.dismiss) var dismiss
+  @State private var showingDeleteAlert = false
+  
+  let book: Book
+  
+  var body: some View {
+    ScrollView {
+      ZStack(alignment: .bottomTrailing) {
+        Image(book.genre ?? "Fantasy")
+          .resizable()
+          .scaledToFit()
+        
+        Text(book.genre?.uppercased() ?? "FANTASY")
+          .font(.caption)
+          .fontWeight(.black)
+          .padding(8)
+          .foregroundColor(.white)
+          .background(.black.opacity(0.75))
+          .clipShape(Capsule())
+          .offset(x: -5, y: -5)
+      }
+      Text(book.author ?? "Unknown")
+        .font(.title)
+        .foregroundColor(.secondary)
+      
+      if let date = book.date {
+        Text(date.formatted(date: .abbreviated, time: .omitted))
+        
+      }
+      
+      Text(book.review ?? "No Review")
+        .padding()
+      
+      RatingView(rating: .constant(Int(book.rating)))
+        .font(.largeTitle)
+    }
+    .navigationTitle(book.title ?? "Unknown Book")
+    .navigationBarTitleDisplayMode(.inline)
+    .alert("Delete book", isPresented: $showingDeleteAlert) {
+      Button("Delete", role: .destructive, action: deleteBook)
+      Button("Cancel", role: .cancel) { }
+    } message: {
+      Text("Are you sure?")
+    }
+    .toolbar {
+      Button {
+        showingDeleteAlert = true
+      } label: {
+        Label("Delete this book", systemImage: "trash")
+      }
+    }
+  }
+  
+  func deleteBook() {
+    moc.delete(book)
+    //add delete functionality here to save
+    dismiss()
+  }
+  
+  func getDate(bookDate: Date) -> String {
+    let dateFormatter = DateFormatter()
+    let dateString = dateFormatter.string(from: bookDate)
+    return dateString
+  }
+}
+
+//
+//struct DetailView_Previews: PreviewProvider {
+//  static let moc = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+//
+//  static var previews: some View {
+//    let book = Book(context: moc)
+//    book.title = "Test book"
+//    book.author = "Test author"
+//    book.genre = "Fantasy"
+//    book.rating = 4
+//    book.review = "This was a great book; I really enjoyed it."
+//
+//    return NavigationView {
+//      DetailView(book: book)
+//    }
+//  }
+//}
