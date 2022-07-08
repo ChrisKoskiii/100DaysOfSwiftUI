c174f71d5c0f1ffae62acca9aab4f2ace0f99fa9
Christopher Koski <82855685+ChrisKoskiii@users.noreply.github.com>
2022-07-08 16:07:45 -0400

    MIlestone10-12 initial commit

---
+++ Milestone10-12/Milestone10-12/Milestone10_12App.swift
@@ -0,0 +1,17 @@
+//
+//  Milestone10_12App.swift
+//  Milestone10-12
+//
+//  Created by Christopher Koski on 7/8/22.
+//
+
+import SwiftUI
+
+@main
+struct Milestone10_12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,7 +1,44 @@
-class Color {
-  var greyscaleValue: Float
-  init(greyscaleValue: Float) {
-    self.greyscaleValue = greyscaleValue
+func printSorted(_ array: [Int]) {
+  // 1
+  guard !array.isEmpty else { return }
+
+  // 2
+  var currentCount = 0
+  var minValue = Int.min
+
+  // 3
+  for value in array {
+    if value == minValue {
+      print(value)
+      currentCount += 1
+    }
   }
-  func set(_ greyScaleValue)
+
+  while currentCount < array.count {
+
+    // 4
+    var currentValue = array.max()!
+
+    for value in array {
+      if value < currentValue && value > minValue {
+        currentValue = value
+      }
+    }
+
+    // 5
+    for value in array {
+      if value == currentValue {
+        print(value)
+        currentCount += 1
+      }
+    }
+
+    // 6
+    minValue = currentValue
+  }
 }
+
+let myInts = [4,5,4,3,12,4,56,3,2]
+printSorted(myInts)
+print(Int.min)
+print(Int.max)
